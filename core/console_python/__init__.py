b0be7e0feecf3cd5e04c47d74c2c7c1a2f36ae6d
baribal <null.s.hoffmann@googlemail.com>
2016-02-22 09:29:48 +0100

    Updated Python console documentation.

--- core/console_python/__init__.py
+++ core/console_python/__init__.py
@@ -2,12 +2,11 @@
 Panda3D offers by itself.
 
 Interface:
-* console: The actual interpreter and GUI
-  * Actually, nothing of this is meant for public consumption ATM.
-* consolew_magic: A set of self-documenting extensions of the
-    console's functionality, in the form of %magic commands.
-  * add_magic (not implemented):
-  * remove_magic (also not implemented):
+* add_magic(magic_name, method): Do not use directly. Instead, derive
+  classes from core.console_python.MagicDonor, decorate its methods
+  with core.console_python.tokenize_magic (zero arguments), and
+  create an instance of that class.
+* remove_magic (not implemented)
 """
 from dbus.proxies import Interface
 
